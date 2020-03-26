67f2894a34dfb4ec5e0fbb554ebdb7ad8238d30b
arka162 <51889278+arka162@users.noreply.github.com>
2020-03-26 19:19:02 +0530

    doing the test run

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyAcinmaUSU3wFqQY1e3tjYJbKu2gMWdg74",
+		authDomain: "my-playground-37eef.firebaseapp.com",
+		databaseURL: "https://my-playground-37eef.firebaseio.com",
+		projectId: "my-playground-37eef",
+		storageBucket: "my-playground-37eef.appspot.com",
+		messagingSenderId: "302683644344",
+		appId: "1:302683644344:web:ae7b0dd13edee19c7d2f4d",
+		measurementId: "G-2FRE90DFTT"
+
+	}
 };
